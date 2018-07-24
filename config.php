ff98bede955a8b2aee02e341db42e2356a02877e
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-07-24 13:50:37 +0800

    整理代码

--- config.php
+++ config.php
@@ -54,20 +54,20 @@ return array(
 
     'support_platform_type' => ['service'], //仅支持微信服务号
 
-    'support_type'  => Ecjia\App\Platform\Plugin\PlatformAbstract::TypeAdmin | Ecjia\App\Platform\Plugin\PlatformAbstract::TypeMerchant,
+    'support_type'          => Ecjia\App\Platform\Plugin\PlatformAbstract::TypeAdmin | Ecjia\App\Platform\Plugin\PlatformAbstract::TypeMerchant,
 			
 	'forms' => array(
-		array('name' => 'point_status',       'type' => 'radiobox',    'value' => ''),
-		array('name' => 'point_value',        'type' => 'text',        'value' => ''),
-		array('name' => 'point_num',          'type' => 'text',        'value' => ''),
-		array('name' => 'point_interval',     'type' => 'select',      'value' => ''),
+		array('name' => 'point_status',         'type' => 'radiobox',    'value' => ''),
+		array('name' => 'point_value',          'type' => 'text',        'value' => ''),
+		array('name' => 'point_num',            'type' => 'text',        'value' => ''),
+		array('name' => 'point_interval',       'type' => 'select',      'value' => ''),
 			
-		array('name' => 'prize_num',          'type' => 'text',        'value' => ''),
-		array('name' => 'starttime',          'type' => 'text',        'value' => ''),
-		array('name' => 'endtime',            'type' => 'text',        'value' => ''),
-		array('name' => 'list',              'type' => 'textarea',    'value' => ''),
-		array('name' => 'description',        'type' => 'textarea',    'value' => ''),
-		array('name' => 'media_id',           'type' => 'text',        'value' => ''),
+		array('name' => 'prize_num',            'type' => 'text',        'value' => ''),
+		array('name' => 'starttime',            'type' => 'text',        'value' => ''),
+		array('name' => 'endtime',              'type' => 'text',        'value' => ''),
+		array('name' => 'list',                 'type' => 'textarea',    'value' => ''),
+		array('name' => 'description',          'type' => 'textarea',    'value' => ''),
+		array('name' => 'media_id',             'type' => 'text',        'value' => ''),
 	),
 );
 
