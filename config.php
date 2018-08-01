734236f01d87718f41d9d377dfe33326a695dacb
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-01 10:00:11 +0800

    移除dzp、ggk、zjd插件的配置项

--- config.php
+++ config.php
@@ -57,17 +57,6 @@ return array(
     'support_type'          => Ecjia\App\Platform\Plugin\PlatformAbstract::TypeAdmin | Ecjia\App\Platform\Plugin\PlatformAbstract::TypeMerchant,
 			
 	'forms' => array(
-		array('name' => 'point_status',         'type' => 'radiobox',    'value' => ''),
-		array('name' => 'point_value',          'type' => 'text',        'value' => ''),
-		array('name' => 'point_num',            'type' => 'text',        'value' => ''),
-		array('name' => 'point_interval',       'type' => 'select',      'value' => ''),
-			
-		array('name' => 'prize_num',            'type' => 'text',        'value' => ''),
-		array('name' => 'starttime',            'type' => 'text',        'value' => ''),
-		array('name' => 'endtime',              'type' => 'text',        'value' => ''),
-		array('name' => 'list',                 'type' => 'textarea',    'value' => ''),
-		array('name' => 'description',          'type' => 'textarea',    'value' => ''),
-		array('name' => 'media_id',             'type' => 'text',        'value' => ''),
 	),
 );
 
