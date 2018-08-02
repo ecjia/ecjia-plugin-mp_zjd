5ca771e2ecb6509fba77575952c490d65d43106c
王正东 <huangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comxhuangyuyuan@ecmoban.comrhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.comyhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comghuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comihuangyuyuan@ecmoban.comlhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-02 10:01:47 +0800

    移除插件中的废弃代码

--- mp_zjd.php
+++ mp_zjd.php
@@ -69,12 +69,6 @@ class plugin_mp_zjd {
         return RC_Api::api('platform', 'plugin_uninstall', $param);
     }
 
-
-    public static function adapter_instance($instance, $config) {
-        require_once RC_Plugin::plugin_dir_path(__FILE__) . 'mp_zjd.class.php';
-        $wechat = new mp_zjd($config);
-        return $wechat;
-    }
 }
 
 Ecjia_PluginManager::extend('mp_zjd', function() {
@@ -84,5 +78,4 @@ Ecjia_PluginManager::extend('mp_zjd', function() {
 
 RC_Plugin::register_activation_hook(__FILE__, array('plugin_mp_zjd', 'install'));
 RC_Plugin::register_deactivation_hook(__FILE__, array('plugin_mp_zjd', 'uninstall'));
-// RC_Hook::add_filter('platform_factory_adapter_instance', array( 'plugin_mp_zjd', 'adapter_instance' ), 10, 2);
 // end
