3e6ca00960374efb9ba806b38c2ccb7274c33120
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-03 21:05:44 +0800

    营销活动只能再微信浏览器中参与

--- templates/zjd_index.dwt.php
+++ templates/zjd_index.dwt.php
@@ -99,13 +99,13 @@
 <script type="text/javascript" src="{$jq_url}"></script>
 <script>
     $(function() {
-//        var ISWeixin = !!navigator.userAgent.match(/MicroMessenger/i); //wp手机无法判断
-//         if(!ISWeixin){
-//             var rd_url = location.href.split('#')[0];  // remove hash
-//             var oauth_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri='+encodeURIComponent(rd_url) + '&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
-//             location.href = oauth_url;
-//             return false;
-//         }
+        var ISWeixin = !!navigator.userAgent.match(/MicroMessenger/i); //wp手机无法判断
+         if(!ISWeixin){
+             var rd_url = location.href.split('#')[0];  // remove hash
+             var oauth_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri='+encodeURIComponent(rd_url) + '&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
+             location.href = oauth_url;
+             return false;
+         }
         var timer,forceStop;
         var wxch_Marquee = function(id){
             try{
