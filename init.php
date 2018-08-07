72a53e2e6ceacc3fc6d47e9921b8c7c7a571081b
张春晨 <huangyuyuan@ecmoban.comzhuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comghuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comuhuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comhhuangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com@huangyuyuan@ecmoban.comehuangyuyuan@ecmoban.comchuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.combhuangyuyuan@ecmoban.comahuangyuyuan@ecmoban.comnhuangyuyuan@ecmoban.com.huangyuyuan@ecmoban.comchuangyuyuan@ecmoban.comohuangyuyuan@ecmoban.commhuangyuyuan@ecmoban.com>
2018-08-07 14:41:09 +0800

    砸金蛋样式修改

--- init.php
+++ init.php
@@ -65,8 +65,8 @@ class mp_zjd_init extends PluginPageController implements PluginPageInterface
 
         $this->assginPluginStyleUrl('egg_1_png', 'images/egg_1.png');
         $this->assginPluginStyleUrl('egg_2_png', 'images/egg_2.png');
-        $this->assginPluginStyleUrl('img_4_png', 'images/img-4.png');
-        $this->assginPluginStyleUrl('img_6_png', 'images/img-6.png');
+        $this->assginPluginStyleUrl('img_4_png', 'images/img_4.png');
+        $this->assginPluginStyleUrl('img_6_png', 'images/img_6.png');
         $this->assginPluginStyleUrl('my_prize_png', 'images/my_prize.png');
         
 
